62f99da1f268167dc8759ac8d5170c389d233bbe
weslei <wesleicasali18@gmail.com>
2023-05-14 22:05:33 -0300

    att

--- controllers/Contas.js
+++ controllers/Contas.js
@@ -24,6 +24,106 @@ const transporter = nodemailer.createTransport({
 
 module.exports = class ContasControllers {
         static login(req,res){
-            res.render('login')
+            if(req.session.userid){
+                res.render('home')
+            } else{
+                res.render('login')
+            }
+        }
+        
+
+        static register(req,res){
+            res.render('register')
+        }
+        
+
+
+        //CRIAR CONTA
+        static async criarConta(req,res){
+            const {nome, email, user, senha, confirm} = req.body
+            
+            //Verifica se as senhas são iguais
+            if(senha != confirm){
+                req.flash('message','As senhas são diferentes!') 
+                res.render('register')
+
+                return
+            }
+
+            
+            //Pega os users do banco
+            const emailTeste = await Users.findOne({raw: true, where: {email: email}})
+            const userTeste = await Users.findOne({raw: true, where: {user: user}})
+            
+
+
+            //Verifica se existe o email no banco
+            if(emailTeste != null){
+                req.flash('message','Email já cadastrado!') 
+                res.render('register')
+
+                return
+            }
+
+            //Verifica se existe o user no banco
+            if(userTeste != null){
+                req.flash('message','Usuário já cadastrado!') 
+                res.render('register')
+    
+                return
+            }
+
+            //Cria o user e salva no banco
+            if(emailTeste == null && userTeste == null){
+                await Users.create({nome, user, senha, email}) 
+                res.redirect('/')
+
+                return
+            }
+        }
+
+
+
+
+        //FAZER LOGIN
+        static async fazerLogin(req,res){
+             const {user, senha} = req.body
+
+             const users = await Users.findAll({raw: true})
+
+             const userTesteLogin = await Users.findOne({raw: true, where: {user: user}})
+             const senhaTesteLogin = await Users.findOne({raw: true, where: {senha: senha}})
+
+             if(userTesteLogin != null && senhaTesteLogin != null){
+                req.session.userid = userTesteLogin.id 
+                    req.session.save(()=>{
+                    res.redirect('/home')
+                })
+             }else{
+                req.flash('messagee','Usuário ou senha incorreto!') 
+                res.render('login')
+             }
+        }
+
+
+
+
+        //HOME
+        static async home(req,res){
+            if(req.session.userid){
+                //Pega os dados do user
+                const dadosUser = await Users.findOne({raw: true, where: {id: req.session.userid}})
+
+                res.render('home')
+            } else{
+                res.render('login')
+            }
+        }
+
+
+        //SAIR
+        static sair(req,res){
+            req.session.destroy()
+            res.redirect('/')
         }
 }
--- routes/contas.js
+++ routes/contas.js
@@ -4,8 +4,22 @@ const router = express.Router()
 const ContasControllers = require('../controllers/Contas')
 
 
-
+//LOGIN E REGISTER
 router.get('/', ContasControllers.login)
+router.get('/register', ContasControllers.register)
+
+//CRIAR CONTA
+router.post('/criarconta', ContasControllers.criarConta)
+
+//Fazer login
+router.post('/fazerlogin', ContasControllers.fazerLogin)
+
+//Home Inicial
+router.get('/home', ContasControllers.home)
+
+//Sair
+router.get('/logout', ContasControllers.sair)
+
 
 
 
